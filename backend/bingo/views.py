14b7fa3e64dc539de30d5c7fda0d7fa0d6689351
Spencer Barnes-Israel <spencer.barnes-israel@kroger.com>
2021-02-09 15:01:48 -0500

    Create views for each endpoint

--- backend/bingo/views.py
+++ backend/bingo/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import BookSerializer, CategorySerializer, SquareSerializer
+from .models import Book, Category, Square
 
 # Create your views here.
+class BookView(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+class CategoryView(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+class SquareView(viewsets.ModelViewSet):
+    serializer_class = SquareSerializer
+    queryset = Square.objects.all()
