a754397c20e6238b4cab4abe8fcaaaccabc17448
Spencer Barnes-Israel <spencer.barnes-israel@kroger.com>
2021-02-09 13:48:28 -0500

    Add Book, Category, and Square models

--- backend/bingo/models.py
+++ backend/bingo/models.py
@@ -5,30 +5,29 @@ from django.utils import timezone
 class Square(models.Model):
     locationX = models.IntegerField()
     locationY = models.IntegerField()
-    categoryId = models.ForeignKey(models.Category)
-    bookId = models.ForeignKey(models.Book)
+    categoryId = models.ForeignKey(models.Category, on_delete=models.CASCADE)
+    bookId = models.ForeignKey(models.Book, on_delete=models.CASCADE)
 
     def __str__(self):
         selfDescribe = str(self.locationX) + ", " + str(self.locationY)
         return selfDescribe
 
 class Book(models.Model):
-    title = models.CharField(max_length=120)
-    author = models.CharField(max_length=120)
-    complete = models.BooleanField(default=False)
-    percentComplete = models.IntegerField()
-    created = models.DateField(default=timezone.now())
-    markedComplete = models.BooleanField(default=False)
-    hardMode = models.BooleanField(default=False)
+    title = models.CharField(max_length=120, help_text="Title of the book")
+    author = models.CharField(max_length=120, help_text="Author of the book")
+    complete = models.BooleanField(default=False, help_text="Field to control completeness of book")
+    percentComplete = models.IntegerField(help_text="Value between 1-100 to indicate percentage complete")
+    created = models.DateField(auto_now=True, help_text="Date book was added to card")
+    hardMode = models.BooleanField(default=False, help_text="Indicates whether or not this book is for HardMode")
 
     def __str__(self):
         return self.title
 
 class Category(models.Model):
-    name = models.CharField(max_length=120)
-    year = models.IntegerField()
-    description = models.TextField()
-    hardmodeDescription = models.TextField()
+    name = models.CharField(max_length=120, help_text="Name of the category")
+    year = models.IntegerField(help_text="Year the category is for")
+    description = models.TextField(help_text="Description from /r/Fantasy official card")
+    hardmodeDescription = models.TextField(help_text="Description from /r/Fantasy official card for HardMode")
 
     def __str__(self):
         return self.name
