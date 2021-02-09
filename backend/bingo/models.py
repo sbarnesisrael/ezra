9663aaafadabec220ce17f3a225074453b6cda53
Spencer Barnes-Israel <spencer.barnes-israel@kroger.com>
2021-02-09 15:01:29 -0500

    Fix typo in models.py

--- backend/bingo/models.py
+++ backend/bingo/models.py
@@ -2,23 +2,13 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-class Square(models.Model):
-    locationX = models.IntegerField()
-    locationY = models.IntegerField()
-    categoryId = models.ForeignKey(models.Category, on_delete=models.CASCADE)
-    bookId = models.ForeignKey(models.Book, on_delete=models.CASCADE)
-
-    def __str__(self):
-        selfDescribe = str(self.locationX) + ", " + str(self.locationY)
-        return selfDescribe
-
 class Book(models.Model):
     title = models.CharField(max_length=120, help_text="Title of the book")
     author = models.CharField(max_length=120, help_text="Author of the book")
     complete = models.BooleanField(default=False, help_text="Field to control completeness of book")
     percentComplete = models.IntegerField(help_text="Value between 1-100 to indicate percentage complete")
-    created = models.DateField(auto_now=True, help_text="Date book was added to card")
     hardMode = models.BooleanField(default=False, help_text="Indicates whether or not this book is for HardMode")
+    created = models.DateField(auto_now=True, help_text="Date book was added to card")
 
     def __str__(self):
         return self.title
@@ -31,3 +21,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Square(models.Model):
+    locationX = models.IntegerField()
+    locationY = models.IntegerField()
+    categoryId = models.ForeignKey(Category, on_delete=models.CASCADE)
+    bookId = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        selfDescribe = str(self.locationX) + ", " + str(self.locationY)
+        return selfDescribe
