7eb59d27427be7dcea7a6b8b3cdf250464af25f1
Spencer Barnes-Israel <spencer.barnes-israel@kroger.com>
2021-02-09 15:01:12 -0500

    Add API urls to urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from bingo import views
+
+router = routers.DefaultRouter()
+router.register(r'books', views.BookView, 'book')
+router.register(r'categories', views.CategoryView, 'category')
+router.register(r'squares', views.SquareView, 'square')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
